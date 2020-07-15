5e545f3b3b37c2122c5967cc362731bae1049471
Fabio Iantas <fb.vini@gmail.com>
2020-07-15 17:33:08 -0300

    make password recover from link

--- src/app/login/forgot-password/forgot-password-boxed.component.ts
+++ src/app/login/forgot-password/forgot-password-boxed.component.ts
@@ -1,6 +1,8 @@
 import { Validators, FormControl, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/_services/authentication.service';
+import { DialogBoxService } from 'src/app/_services/dialog-box.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password-boxed',
@@ -13,7 +15,9 @@ export class ForgotPasswordBoxedComponent implements OnInit {
     email: new FormControl('', Validators.required)
   });
 
-  constructor(private authService: AuthenticationService) { }
+  constructor(private authService: AuthenticationService,
+             private dialogBox: DialogBoxService,
+             private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,6 +25,8 @@ export class ForgotPasswordBoxedComponent implements OnInit {
   recoverPass() {
     this.authService.recover(this.formGroup.value).subscribe(data => {
       console.log(JSON.stringify(data));
+      this.dialogBox.show('Instruções de recuperação enviada para o e-mail informado.','WARNING');
+        this.router.navigate(['/login']);
     });
   }
 
