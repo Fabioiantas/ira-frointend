c1367c17641ff4b9f9277d64b0a1e6f4f21a8e94
Fabio Iantas <fb.vini@gmail.com>
2021-03-13 15:28:02 -0300

    heroku config

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
-const express = require('express');
-const app = express();
-const path = required('path');
+/* Arquivo para criação de um servidor local
+ * utilizado para executar o código de produção.
+ *
+ * @author Márcio Casale de Souza <contato@kazale.com>
+ * @since 0.0.6
+ */
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-})
-console.log('Console listening!');
+var path = require('path');
+var express = require('express');
+var app = express();
+var cors = require('cors');
+app.use(cors());
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// redireciona todas as requições para o Angular 2
+app.all('*', function(req, res) {
+  res.status(200).sendFile(
+  	path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node executando na porta ', app.get('port'));
+});
