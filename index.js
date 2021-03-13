5f334d3e3d928647f65440a57eb43d04f4323cec
Fabio Iantas <fb.vini@gmail.com>
2021-03-13 16:43:07 -0300

    server dist

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-/* Arquivo para criação de um servidor local
- * utilizado para executar o código de produção.
- *
- * @author Márcio Casale de Souza <contato@kazale.com>
- * @since 0.0.6
- */
-
 var path = require('path');
 var express = require('express');
 var app = express();
@@ -13,12 +6,12 @@ app.use(cors());
 
 app.set('port', (process.env.PORT || 5000));
 
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist/architectui-angular-pro')));
 
 // redireciona todas as requições para o Angular 2
 app.all('*', function(req, res) {
   res.status(200).sendFile(
-  	path.join(__dirname, 'dist', 'index.html'));
+  	path.join(__dirname, 'dist/architectui-angular-pro', 'index.html'));
 });
 
 app.listen(app.get('port'), function() {
--- server.js
+++ server.js
@@ -1,10 +1,3 @@
-/* Arquivo para criação de um servidor local
- * utilizado para executar o código de produção.
- *
- * @author Márcio Casale de Souza <contato@kazale.com>
- * @since 0.0.6
- */
-
 var path = require('path');
 var express = require('express');
 var app = express();
@@ -15,7 +8,6 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// redireciona todas as requições para o Angular 2
 app.all('*', function(req, res) {
   res.status(200).sendFile(
   	path.join(__dirname, 'dist', 'index.html'));
